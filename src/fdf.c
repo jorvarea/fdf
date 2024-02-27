e63c40b94a42134ae20909e3e4f655b44587dcc8
jorvarea <jorvarea@users.noreply.github.com>
2024-02-27 16:18:34 +0100

    first mlx test

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 22:53:57 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/02/25 18:33:28 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/02/27 14:58:38 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int	main(int argc, char **argv)
 {
 	t_map	map;
+	mlx_t	*window;
 
 	check_argument_count_error(argc);
 	ft_memset(&map, 0, sizeof(t_map));
 	parse_map(argv[1], &map);
-	print_map_array(&map);
-	print_colors(&map);
+	window = mlx_init(1000, 1000, "Test Window", true);
+	mlx_loop(window);
+	mlx_terminate(window);
 	free_map_memory(&map);
 	return (0);
 }

012e0a0b1efe0f0a0ced97ef7f5ede6fed50aa9a
jorvarea <jorvarea@users.noreply.github.com>
2024-03-26 16:49:04 +0100

    now it prints the xy proyection of maps

--- src/fdf.c
+++ src/fdf.c
@@ -6,51 +6,84 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 22:53:57 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/03/02 20:35:09 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/03/26 16:44:19 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	top_view(mlx_t *mlx, t_map *map)
+void draw_line_between_points(mlx_image_t *img, t_point a, t_point b)
 {
-	int			i;
-	uint32_t	x;
-	uint32_t	y;
-	int			xspacing;
-	int			yspacing;
-	mlx_image_t	*img;
-
-	img = mlx_new_image(mlx, mlx->width * (1 - 2 * WINDOW_MARGIN), mlx->height * (1 - 2 * WINDOW_MARGIN));
-	xspacing = (mlx->width * (1 -  2 * WINDOW_MARGIN)) / map->ncols;
-	yspacing = (mlx->height * (1 - 2 * WINDOW_MARGIN)) / map->nrows;
-	i = 0;
-	y = 0;
-	while (i < map->nrows)
+	int slope;
+	int x;
+	int y;
+	
+	if (b.x != a.x)
 	{
-		x = 0;
-		while (x < img->width)
+		x = a.x;
+		slope = (b.y - a.y) / (b.x - a.x);
+		while (x <= b.x)
 		{
+			y = a.y + slope * (x - a.x);
 			mlx_put_pixel(img, x, y, 0xFFFFFFFF);
 			x++;
 		}
-		i++;
-		y += yspacing;
 	}
-	i = 0;
-	x = 0;
-	while (i < map->ncols)
+	else
 	{
-		y = 0;
-		while (y < img->height)
+		x = a.x;
+		y = a.y;
+		while (y <= b.y)
 		{
 			mlx_put_pixel(img, x, y, 0xFFFFFFFF);
 			y++;
 		}
-		i++;
-		x += xspacing;
 	}
-	mlx_image_to_window(mlx, img, mlx->width * WINDOW_MARGIN, mlx->height * WINDOW_MARGIN);
+}
+
+void connect_neighbours(mlx_image_t *img, int fil, int col, int spacing)
+{
+	t_point current_point;
+	t_point neighbour;
+	
+	current_point.x = col * spacing;
+	current_point.y = fil * spacing;
+	if (fil != 0)
+	{
+		neighbour.x = current_point.x;
+		neighbour.y = current_point.y - spacing;
+		draw_line_between_points(img, neighbour, current_point);
+	}
+	if (col != 0)
+	{
+		neighbour.x = current_point.x - spacing;
+		neighbour.y = current_point.y;
+		draw_line_between_points(img, neighbour, current_point);
+	}
+}
+
+void	top_view(mlx_t *mlx, t_map *map)
+{
+	int	i;
+	int j;
+	int	spacing;
+	mlx_image_t	*img;
+
+	img = mlx_new_image(mlx, mlx->width, mlx->height);
+	check_mlx_image_error(img);
+	spacing = ft_min_float(img->width / map->ncols, img->height / map->nrows);
+	i = 0;
+	while (i < map->nrows)
+	{
+		j = 0;
+		while (j < map->ncols)
+		{
+			connect_neighbours(img, i, j, spacing);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(mlx, img, 0, 0);
 }
 
 void	manage_key_pressed(void *mlx)
@@ -67,6 +100,7 @@ int	main(int argc, char **argv)
 	check_argument_count_error(argc);
 	ft_memset(&map, 0, sizeof(t_map));
 	parse_map(argv[1], &map);
+	check_valid_map_dimensions(&map);
 	mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Fil de fer", true);
 	check_mlx_error(mlx);
 	top_view(mlx, &map);

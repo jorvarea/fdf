7f2fec524fa094bb93b0cf2563bcf82604bb97f3
jorvarea <jorvarea@users.noreply.github.com>
2024-03-27 15:57:28 +0100

    changed window size

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 22:33:48 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/03/27 01:59:03 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/03/27 15:56:37 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 
 # define INITIAL_MAP_DIMENSION 10
 # define INITIAL_COLORS_DIMENSION 20
-# define WINDOW_WIDTH 1000
-# define WINDOW_HEIGHT 700
+# define WINDOW_WIDTH 1400
+# define WINDOW_HEIGHT 800
 # define ZOOM_UNIT 0.01
 # define ZOOM_LIMIT 3
 

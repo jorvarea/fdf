818711a0f887e48450bc74f01a0eed0a7ed6c853
jorvarea <jorvarea@users.noreply.github.com>
2024-04-06 14:23:47 +0200

    minor name fix

--- src/calculate_rotation_matrix.c
+++ src/calculate_rotation_matrix.c
@@ -6,14 +6,13 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 13:03:29 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/04/06 13:29:58 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/04/06 14:23:22 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	calculate_x_rotation_matrix(t_rot_matrix *rot_matrix,
-		float theta_x)
+void	calculate_x_rot_matrix(t_rot_matrix *rot_matrix, float theta_x)
 {
 	rot_matrix->m[0][0] = 1;
 	rot_matrix->m[0][1] = 0;
@@ -26,8 +25,7 @@ void	calculate_x_rotation_matrix(t_rot_matrix *rot_matrix,
 	rot_matrix->m[2][2] = cos(theta_x);
 }
 
-void	calculate_y_rotation_matrix(t_rot_matrix *rot_matrix,
-		float theta_y)
+void	calculate_y_rot_matrix(t_rot_matrix *rot_matrix, float theta_y)
 {
 	rot_matrix->m[0][0] = cos(theta_y);
 	rot_matrix->m[0][1] = 0;
@@ -40,8 +38,7 @@ void	calculate_y_rotation_matrix(t_rot_matrix *rot_matrix,
 	rot_matrix->m[2][2] = cos(theta_y);
 }
 
-void	calculate_z_rotation_matrix(t_rot_matrix *rot_matrix,
-		float theta_z)
+void	calculate_z_rot_matrix(t_rot_matrix *rot_matrix, float theta_z)
 {
 	rot_matrix->m[0][0] = cos(theta_z);
 	rot_matrix->m[0][1] = -sin(theta_z);

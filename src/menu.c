9f8a9f2018a70c5ce600e91c073b484617bbcbc8
jorvarea <jorvarea@c3r9s2.42malaga.com>
2024-04-13 22:57:37 +0200

    updated menu

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 00:37:30 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/04/11 00:39:05 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/04/13 22:57:01 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	print_menu(mlx_t *mlx)
 	mlx_put_string(mlx, "Rotate X: press 'A' or 'Z'", 50, y += 20);
 	mlx_put_string(mlx, "Rotate Y: press 'S' or 'X'", 50, y += 20);
 	mlx_put_string(mlx, "Rotate Z: press 'D' or 'Z'", 50, y += 20);
-	mlx_put_string(mlx, "Scale Z: press 'Q' or 'W'", 50, y += 20);
 	mlx_put_string(mlx, "To change projection view:", 50, y += 35);
 	mlx_put_string(mlx, "Isometric: press '1'", 50, y += 20);
 	mlx_put_string(mlx, "Perspective: press '2'", 50, y += 20);

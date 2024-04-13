500f7017c6a9880adf5bd6100fde0d2863c824d4
jorvarea <jorvarea@c3r9s2.42malaga.com>
2024-04-13 22:47:57 +0200

    zooming from the center of the image

--- src/manage_key_zoom.c
+++ src/manage_key_zoom.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:51:47 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/04/13 21:52:04 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/04/13 22:45:54 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 void	manage_zoom_in(t_state *state)
 {
-	t_point	focus;
+	t_point		focus;
+	mlx_image_t	*img;
+	t_point		**m;
+	int			nrows;
+	int			ncols;
 
-	focus.xyz[0] = 0;
-	focus.xyz[1] = 0;
+	img = state->img;
+	nrows = state->coord_matrix->nrows;
+	ncols = state->coord_matrix->ncols;
+	m = state->coord_matrix->m;
+	focus.xyz[0] = m[nrows / 2][ncols / 2].xyz[0] + (img->width / 2 - m[nrows
+			/ 2][ncols / 2].xyz[0]);
+	focus.xyz[1] = m[nrows / 2][ncols / 2].xyz[1] + (img->height / 2 - m[nrows
+			/ 2][ncols / 2].xyz[1]);
 	perform_zoom(state->coord_matrix, ZOOM_FACTOR, &focus);
 	mlx_delete_image(state->mlx, state->img);
 	state->img = redraw_image(state->mlx, state->coord_matrix);
@@ -25,10 +35,20 @@ void	manage_zoom_in(t_state *state)
 
 void	manage_zoom_out(t_state *state)
 {
-	t_point	focus;
+	t_point		focus;
+	mlx_image_t	*img;
+	t_point		**m;
+	int			nrows;
+	int			ncols;
 
-	focus.xyz[0] = 0;
-	focus.xyz[1] = 0;
+	img = state->img;
+	nrows = state->coord_matrix->nrows;
+	ncols = state->coord_matrix->ncols;
+	m = state->coord_matrix->m;
+	focus.xyz[0] = m[nrows / 2][ncols / 2].xyz[0] + (img->width / 2 - m[nrows
+			/ 2][ncols / 2].xyz[0]);
+	focus.xyz[1] = m[nrows / 2][ncols / 2].xyz[1] + (img->height / 2 - m[nrows
+			/ 2][ncols / 2].xyz[1]);
 	perform_zoom(state->coord_matrix, 1.0 / ZOOM_FACTOR, &focus);
 	mlx_delete_image(state->mlx, state->img);
 	state->img = redraw_image(state->mlx, state->coord_matrix);

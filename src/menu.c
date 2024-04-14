ea94184654795190e6bcd9c054716826312993ec
jorvarea <jorvarea@c1r9s3.42malaga.com>
2024-04-14 20:58:05 +0200

    minor fix

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 00:37:30 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/04/14 20:44:31 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/04/14 20:57:34 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ void	print_menu(mlx_t *mlx)
 	mlx_put_string(mlx, "Rotate Y: press 'S' or 'X'", 50, y += 20);
 	mlx_put_string(mlx, "Rotate Z: press 'D' or 'Z'", 50, y += 20);
 	mlx_put_string(mlx, "To change projection view:", 50, y += 35);
-	mlx_put_string(mlx, "Press '1', '2', '3' or '4", 50, y += 20);
+	mlx_put_string(mlx, "Press '1', '2', '3' or '4'", 50, y += 20);
 	mlx_put_string(mlx, "Reset view: press 'R'", 50, y += 35);
 }
